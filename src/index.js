74c0db09f7d8e320baaa20ec050e20069f9bff2e
Sophia <qyoung@ucsd.edu>
2018-10-24 23:06:56 -0700

    small fix on file address
    changed bootstrap address in index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import '/Users/jordentang/chimein/src/bootstrap.min.css'
+import '../src/bootstrap.min.css'
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
