0e151d80b2cfd5bb05c2c48cb6fd63257533f022
Sophia <qyoung@ucsd.edu>
2018-10-27 15:50:20 -0700

    dashboard test

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+//import App from './App';
 import * as serviceWorker from './serviceWorker';
 import '../src/bootstrap.min.css'
+import App from './drawers';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
