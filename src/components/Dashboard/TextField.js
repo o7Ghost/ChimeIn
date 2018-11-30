6a186b01226ae750f463d8559ec9e7f753b74f3c
Pwc <p6cheng@ucsd.edu>
2018-11-30 03:10:38 -0800

    Cooldown Finished!

--- src/components/Dashboard/TextField.js
+++ src/components/Dashboard/TextField.js
@@ -5,6 +5,10 @@ import { withStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 
+import IconButton from "@material-ui/core/IconButton/IconButton";
+import CloseIcon from "@material-ui/core/SvgIcon/SvgIcon";
+import Snackbar from "@material-ui/core/Snackbar/Snackbar";
+
 const styles = theme => ({
     container: {
         display: 'flex',
@@ -22,6 +26,16 @@ class TextFields extends React.Component {
     constructor(props) {
         super(props);
 
+        this.statics = {
+            cooldowntime: 60000  // change this field to change cooldown time
+        };
+
+        this.state = {
+            buttonDisabled: false,
+            open: false,
+            notification: '',
+            submitText: 'Submit'
+        };
 
         this.firebaseRef = this.props.db.database().ref("ClassFinal");
         var classRef = this.firebaseRef.child(this.props.curClass);
@@ -32,6 +46,7 @@ class TextFields extends React.Component {
     componentWillUnmount() {
         this.firebaseRef.off();
     }
+
     //a method to push to firebase and then clean user input
     pushToFirebase(event) {
         const {UID, Question, upvoteCount, order, Answer, timestamp, followers} = this.props.value;
@@ -40,11 +55,22 @@ class TextFields extends React.Component {
         time = time.toJSON().split(".")[0];
         var cID = this.props.db.auth().currentUser.uid;
 
-        if(Question != '') {
+        if(Question != '' && this.state.buttonDisabled == false) {
             this.firebaseRef.child( cID + "+" + time).set({UID: cID,
                 Question: Question, upvoteCount: upvoteCount, order:order, timestamp: time, followers: this.props.value.followers});
-        }
 
+            // update user lastPostTime
+            var userRef = this.props.db.database().ref("User").child(this.props.db.auth().currentUser.uid);
+            userRef.update({lastPostTime: time});
+
+            // after this submission, set button disabled with timeout function, 1s = 1000
+            this.setState({buttonDisabled: true, submitText: 'Wait!'});
+            setTimeout(() => this.setState({ buttonDisabled: false, submitText: 'Submit' }), this.statics.cooldowntime);
+        }
+        else if(Question == '' && this.state.buttonDisabled == false) {
+            this.state.notification = 'Can not submit blank question';
+            this.state.open = true;
+        }
         this.props.stateChange('');
     }
     /*
@@ -57,11 +83,24 @@ class TextFields extends React.Component {
 
     _handleKeyPress = (e) => {
         if (e.key === 'Enter' && e.shiftKey) {
-            this.pushToFirebase();
             e.preventDefault();
-            this.setState({Question: '', upvoteCount: 0});
+            if(this.state.buttonDisabled == false) {
+                this.pushToFirebase();
+                this.setState({Question: '', upvoteCount: 0});
+            }
+            else {
+                this.setState({notification: 'Please wait at least one minute before you can submit another question.', open: true});
+            }
         }
     }
+
+    handleClose = (event, reason) => {
+        if (reason === "clickaway") {
+            return;
+        }
+
+        this.setState({ open: false });
+    };
 
 
 //, upvoteCount: this.props.value.upvoteCount
@@ -74,8 +113,43 @@ class TextFields extends React.Component {
         this.questionRef = this.classRef.child("questions");
         this.firebaseRef=this.questionRef;
 
+        var lastPostDate = null;
+        var hour = null;
+        var minute = null;
+        var second = null;
+        var userRef = this.props.db.database().ref("User").child(this.props.db.auth().currentUser.uid);
+        userRef.on('value', (snapshot) => {
+            var posttime = snapshot.val().lastPostTime;
+            lastPostDate = posttime.split("T")[0];
+            const lastPostTime = posttime.split("T")[1];
+            hour = lastPostTime.split(":")[0];
+            minute = lastPostTime.split(":")[1];
+            var temp = lastPostTime.split(":")[2];
+            second = temp.split("Z")[0];
+            //console.log("aaaaaaa" + hour+minute+second);
+
+            var time = new Date();
+            time = time.toJSON().split(".")[0];
+            var curPostDate = time.split("T")[0];
+            var curhour = time.split(":")[0];
+            var curminute = time.split(":")[1];
+            var cursecond = time.split(":")[2].split("Z")[0];
+
+            var curTime = parseInt(curminute+cursecond);
+            var lastTime = parseInt(minute+second);
+
+            var diff = curTime - lastTime;
+            console.log("oooo"+ diff);
+            if(diff < this.statics.cooldowntime/1000 && diff > 0) {
+                this.state.buttonDisabled = true;
+                this.state.submitText = 'Wait!';
+                setTimeout(() => this.setState({ buttonDisabled: false, submitText: 'Submit' }), (this.statics.cooldowntime - diff*1000));
+            }
+        });
+
         const { classes } = this.props;
         return (
+            <div>
             <form className={classes.container} noValidate autoComplete="off">
                 <TextField value = { this.props.value.Question}
                            id="outlined-multiline-flexible"
@@ -90,11 +164,44 @@ class TextFields extends React.Component {
                            onKeyPress={this._handleKeyPress}/>
 
                 <Button variant="outlined" id="submitButton" href="#" className={classes.button}
-                        onClick={this.pushToFirebase.bind(this)}>
-                    Submit
+                        onClick={this.pushToFirebase.bind(this)} disabled={this.state.buttonDisabled}>
+                    {this.state.submitText}
                 </Button>
             </form>
 
+                <Snackbar
+                    anchorOrigin={{
+                        vertical: "top",
+                        horizontal: "right"
+                    }}
+                    open={this.state.open}
+                    autoHideDuration={6000}
+                    onClose={this.handleClose}
+                    ContentProps={{
+                        "aria-describedby": "message-id"
+                    }}
+                    message={<span id="message-id">{this.state.notification}</span>}
+                    action={[
+                        <Button
+                            key="undo"
+                            color="secondary"
+                            size="small"
+                            onClick={this.handleClose}
+                        >
+                            Close
+                        </Button>,
+                        <IconButton
+                            key="close"
+                            aria-label="Close"
+                            color="inherit"
+                            className={classes.close}
+                            onClick={this.handleClose}
+                        >
+                            <CloseIcon />
+                        </IconButton>
+                    ]}
+                />
+            </div>
         );
     }
 
