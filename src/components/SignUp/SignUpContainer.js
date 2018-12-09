bff649b508a4f7d775291a2648f5f741915b269c
Pwc <p6cheng@ucsd.edu>
2018-12-08 21:22:39 -0800

    Signup time fixed

--- src/components/SignUp/SignUpContainer.js
+++ src/components/SignUp/SignUpContainer.js
@@ -30,8 +30,9 @@ class SignUpContainer extends Component {
             var time = new Date();
             var firebaseRef = firebase.database().ref("User");
             firebaseRef.child(firebase.auth().currentUser.uid).set({
-                lastPostTime: time.toJSON(), micOff: time.toJSON(),
-                projectorOff: time.toJSON(), writing: time.toJSON(), coolDown: time.toJSON()
+                lastPostTime: new Date('2000-01-01T00:00:59.207Z').toJSON(), micOff: new Date('2000-01-01T00:00:59.207Z').toJSON(),
+                projectorOff: new Date('2000-01-01T00:00:59.207Z').toJSON(), writing: new Date('2000-01-01T00:00:59.207Z').toJSON(),
+                coolDown: new Date('2000-01-01T00:00:59.207Z').toJSON()
             });
             var firebaseRef2 = firebase.database().ref("UserByEmail");
             var emailTemp = email.value.replace("@"," ");
